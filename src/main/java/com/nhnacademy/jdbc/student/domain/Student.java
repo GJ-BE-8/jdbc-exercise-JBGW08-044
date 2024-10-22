cd1fc7de059b1a5c5a5f63bdf23c219758fb5631
Chae Noah <canoa4943@gmail.com>
2024-10-22 17:26:01 +0900

    임시저장

--- src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
+++ src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
@@ -1,13 +1,19 @@
 package com.nhnacademy.jdbc.student.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Getter
+@Setter
 public class Student {
 
     public enum GENDER{
         M,F
     }
+
     private final String id;
     private final String name;
     private final GENDER gender;
@@ -15,4 +21,17 @@ public class Student {
     private final LocalDateTime createdAt;
 
     //todo#0 필요한 method가 있다면 추가합니다.
+//    public Student() {
+//
+//    }
+
+    public Student(String id, String name, GENDER gender, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    //lombok에 있는 @Getter와 @Setter 어노테이션으로 나머지 매서드 대체
 }
